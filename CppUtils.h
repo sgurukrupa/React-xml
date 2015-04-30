59c38d53db726eef2ba3ccbae628685609f7635a
Ganesh A Hegde <sgurukrupa@gmail.com>
2015-04-30 17:39:50 +0530

    Create CppUtils.h
    Haribol.

---
+++ CppUtils.h
@@ -0,0 +1,16 @@
+#pragma once
+#ifdef _CPPUTILS_
+#define LIB_EXPORTS __declspec(dllexport)
+#elif _USE_CPPUTILS_
+#define LIB_EXPORTS __declspec(dllimport)
+#else
+#define LIB_EXPORTS
+#endif
+
+#include <tchar.h>
+#include <string>
+
+namespace cpputils
+{
+    typedef std::basic_string<TCHAR> tstring;
+}
