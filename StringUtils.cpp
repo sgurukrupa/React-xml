0dae2bdaf70f91deccdde75c7e0bc9e83679e232
Ganesh A Hegde <sgurukrupa@gmail.com>
2015-04-30 17:39:03 +0530

    Create StringUtils.cpp
    Haribol.

---
+++ StringUtils.cpp
@@ -0,0 +1,30 @@
+#include "StringUtils.h"
+
+namespace StringUtils
+{
+    cpputils::tstring Format(const TCHAR * const fmt, ...)
+    {
+        va_list a;
+        va_start(a, fmt);
+        TCHAR buf[100];
+        bool done = false;
+        done = _vsntprintf(buf, sizeof buf, fmt, a) < sizeof buf;
+        va_end(a);
+        return done ? buf : _T("");
+    }
+
+    bool Tokenizer::NextToken()
+    {
+        if (source.empty()) return false;
+        auto ix = source.find_first_of(delim, 0);
+        if (ix == tstring::npos) ix = source.length();
+        token = source.substr(0, ix);
+        source.erase(0, ix + 1); // erase the delimiter too
+        return true;
+    }
+
+    const tstring &Tokenizer::GetToken()
+    {
+        return token;
+    }
+}
